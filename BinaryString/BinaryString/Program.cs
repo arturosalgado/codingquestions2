7ad9d95c3cf53c398cfc65f9377df4be51b10cdd
arturo de los angeles <arturodelosangeles@hotmail.com>
2014-12-09 21:57:30 -0600

    string - backtracking

---
+++ BinaryString/BinaryString/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BinaryString
+{
+    class Program
+    {
+        static  int[] a = { 5,7};
+        static void Main(string[] args)
+        {
+           
+            print(a);
+            binary(a.Length);
+        }
+
+        static void binary(int n)
+        {
+            if (n < 1)
+                print(a);
+            else {
+                a[n-1]=0;
+                binary(n-1);
+                a[n - 1] = 1;
+                binary(n - 1);
+            }
+        }
+
+        static void print(int []a)
+        {
+            foreach (int p in a)
+            {
+                Console.Write("{0} ",p);
+            }
+            Console.WriteLine();
+        }
+
+    }
+
+}
