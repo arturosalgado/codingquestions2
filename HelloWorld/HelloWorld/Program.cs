60db67cafe66800e3e6e4f468f298b04ac60b02c
arturo <arturodelosangeles@hotmail.com>
2015-02-06 02:17:06 -0600

    alg3

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -11,8 +11,9 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Arturo");
-            int[] a = { 6,5,4,3,2,1};
-            InsertionSort(a);
+            int[] a = {6,5,4,3};
+            shift(a,2);
+            //InsertionSort2(a);
             foreach (var t in a) {
                 Console.Write("{0} ",t);
             }
@@ -33,6 +34,43 @@ namespace HelloWorld
                 a[j] = e;
             }
         }
+        static void InsertionSort2(int[] a)
+        {
+            for (int i = 1; i < a.Length; i++)
+            { 
+                int e = a[i];
+                int j = i;
+                while(j>0 && e<a[j-1])
+                {
+                    a[j]=a[j-1];
+                    j--;
+                }
+                a[j] = e;
+                //p(a);
+            }
+        }
+
+        static void shift(int[] a, int j)
+        {
+            p(a);
+            while(j>0){
+                a[j]= a[j-1];
+                j--;
+                p(a);
+                //Console.ReadKey();
+            }
+
+        }
+
+        static void p(int []a)
+        {
+            foreach (var t in a)
+            {
+                Console.Write("{0} ", t);
+            }
+            Console.WriteLine();
+        }
+
 
 
     }
---
+++ SortingAlgorithms/SortingAlgorithms/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int []a = {4};
+            foreach (var t in a)
+            {
+                Console.Write("{0} ", t);
+            }
+            SelectionSort(a);
+            foreach (var t in a) {
+             Console.Write("{0} ",t);
+            }
+            Console.ReadKey();
+
+        }
+
+        static void SelectionSort(int []a)
+        {
+            int min = 0;
+
+
+            for (int runner = 0; runner < a.Length; runner++ )
+            {
+                int t = runner;
+                while (t < a.Length)
+                {
+                    if (a[t] < a[min])
+                    {
+                        min = t;
+                    }
+                    t++;
+                }
+                swap(a, runner, min);// temporarily arbitrarily at 0
+            }
+
+        }
+        static void swap(int []a, int p, int q)
+        {
+            int temp = a[p];
+            a[p] = a[q];
+            a[q] = temp;
+        }
+
+
+    }
+}
