a1ed5592b9759a82c67667291be71ba6de984caf
arturo <arturodelosangeles@hotmail.com>
2015-02-14 19:33:09 -0600

    more files

--- WhoWinsTheElection1/WhoWinsTheElection1/Program.cs
+++ WhoWinsTheElection1/WhoWinsTheElection1/Program.cs
@@ -10,6 +10,31 @@ namespace WhoWinsTheElection1
     {
         static void Main(string[] args)
         {
+            int []a ={1,3,4,5,5,6,5,5,2,3,1,4,2,1};
+            Console.WriteLine("winner {0}", winner(a));
+            Console.ReadKey();
+        }
+
+        static int winner(int[] a)
+        {
+            int count=0;
+            int max=0;
+            int candidate=0;
+            for(int p =0; p<a.Length; p++)
+            {
+                count = 0;
+                for(int j = p + 1; j < a.Length; j++) {
+                    if (a[p] == a[j])
+                        count++;
+                }
+                if (count > max)
+                {
+                    max = count;
+                    candidate = a[p];//stays with it, until a new max comes
+                }
+            }
+
+            return candidate;
         }
     }
 }
---
+++ WhoWinsTheElection2/WhoWinsTheElection2/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhoWinsTheElection2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 2,1,1 };
+            Console.WriteLine("winner-1 {0}", winner(a));
+            Console.WriteLine("winner-2 {0}", winner2(a));
+
+            Console.ReadKey();
+        }
+        static void pr(int[] a)
+        {
+            foreach (int t in a) {
+                Console.Write(" {0}",t);
+            }
+        }
+        static int winner(int[] a)
+        {
+            // sort by any means, take into account complexity
+            Array.Sort(a);
+            pr(a);
+            int candidate = a[0];
+            int counter =1;
+            int max = 1;
+            int maxCandidate = a[0];
+            for (int p = 1; p < a.Length; p++)
+            {
+                if (a[p] == candidate)
+                {
+                    counter++;
+                }
+                else {
+                    candidate = a[p];
+                    counter = 1;
+                    
+                }
+
+                if (counter > max)
+                {
+                    max = counter;
+                    maxCandidate = candidate;
+                }
+            }
+
+            return maxCandidate;
+        }
+        static int winner2(int[] a)
+        {
+            // sort by any means, take into account complexity
+            Array.Sort(a);
+            pr(a);
+            int candidate = a[0];
+            int counter = 1;
+            int max = 1;
+            int maxCandidate = a[0];
+            for (int p = 0; p < a.Length; p++)
+            {
+                if (a[p] == candidate)
+                {
+                    counter++;
+                    
+                }
+                else
+                {
+                    candidate = a[p];
+                    counter = 1;
+                   
+                }
+
+                if (counter > max)
+                {
+                    max = counter;
+                    maxCandidate = candidate;
+                }
+               
+            }
+
+            return maxCandidate;
+        }
+    }
+}
